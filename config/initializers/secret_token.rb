bbc7a885b0c5b203eb5e76d62dfaff8fc9b32334
Hollin Wilkins <hrw7@cornell.edu>
2011-06-30 16:50:39 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birthday::Application.config.secret_token = '6c9db02784dbbb301d62c91a50354c471ef0e7658ed4d9130ba5fa1a3875403be14227201d0cd2faa9ec899676d5f04fc0c3f20ae4d79eda3fcc0fa7b60b80b2'
